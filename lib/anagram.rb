9ae1116edb23c7990e2f467e16b625142ba55364
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-27 21:54:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,10 @@ class Anagram
     
     def match(word_array)
       word_array.select do |word_from_array| #select will make array of words that apply
-      (@word.split("").sort) == (word_from_array.split("").sort) #iterates through word_array and compares to word.
+      @word.split("").sort == word_from_array.split("").sort 
+      #iterates through word_array and compares to word.
+      #[1, 3, 2].sort == [3, 2, 1].sort => true (remember!!)
+
     end
     end
   end
